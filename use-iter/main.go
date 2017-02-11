a9de941e769ddbe4511bbf8260d996826b25e2be
ktrysmt <kotaro.yoshimatsu@gmail.com>
2017-02-11 13:34:46 +0900

    understand iterator

--- use-iter/main.go
+++ use-iter/main.go
@@ -5,5 +5,18 @@ import (
 )
 
 func main() {
-	fmt.Println("test")
+	fmt.Println("---")
+	fmt.Println(num(9))
+
+	for i := range num(9) {
+		fmt.Println(i)
+	}
+
+	for i, v := range make([]int, 9) {
+		fmt.Println(i, v)
+	}
+}
+
+func num(n int) []int {
+	return make([]int, n)
 }
